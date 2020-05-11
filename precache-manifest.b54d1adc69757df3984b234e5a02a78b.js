70218cab9ca75c50ab5e8cc75d095bcf004f6a1b
Shashank Shekhar <sshekhar336@gmail.com>
2020-05-11 05:51:57 +0530

    Updates

---
+++ precache-manifest.b54d1adc69757df3984b234e5a02a78b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d74a976ef80939411ecd2c03996c30e",
+    "url": "/dashboard-draggable/index.html"
+  },
+  {
+    "revision": "f1a59d4efca7fe01e831",
+    "url": "/dashboard-draggable/static/css/main.aa87e827.chunk.css"
+  },
+  {
+    "revision": "82c2ab9196c209f43bc7",
+    "url": "/dashboard-draggable/static/js/2.d521ab4c.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/dashboard-draggable/static/js/2.d521ab4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a59d4efca7fe01e831",
+    "url": "/dashboard-draggable/static/js/main.6081ba7f.chunk.js"
+  },
+  {
+    "revision": "bf3a3fa4aee4c0af8c99",
+    "url": "/dashboard-draggable/static/js/runtime-main.3de29de2.js"
+  }
+]);
